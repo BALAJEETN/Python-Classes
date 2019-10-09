2b502a3ae563f818d9c342b4a86f862a7aa03a07
Balajee <balajee@balajee.com>
2019-10-09 23:38:54 +0530

    test

---
+++ Exercise1.py
@@ -0,0 +1,27 @@
+#Exercise 1
+print ("Howdy")
+
+#Exercise 2
+N= "boy"
+m= "girl"
+print (N,m)
+
+#Exercise 3
+def number_formats ():
+    print(8.73)
+    print(oct(873))
+    print(hex(873))
+number_formats()
+
+#Exercise 4
+print("I think. Therefore I'm")
+
+#Exercise 5
+print('"All is fair in love and war"')
+
+#Exercise 6
+print('"I shall master \n the python language"')
+
+
+
+
